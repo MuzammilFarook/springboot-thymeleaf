77f494b9844cdf1af043f53063ef756acc2fe563
Muzammil <mailmuzam@gmail.com>
2018-10-15 16:10:13 +0100

    Update HelloController.java

--- src/main/java/com/hellokoding/springboot/HelloController.java
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -1,29 +1,27 @@
 package com.hellokoding.springboot;
 import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String hello(Model model) throws  IOException{
-       	    String shutdownCommand;
-		    String operatingSystem = System.getProperty("os.name");
+	@RequestMapping("/shutdown")
+	public String hello(Model model) throws IOException {
+		String shutdownCommand;
+		String operatingSystem = System.getProperty("os.name");
+		System.out.println(operatingSystem + "Operating sys");
+		if (operatingSystem.contains("Linux") || operatingSystem.contains("Mac OS X")) {
+			shutdownCommand = "shutdown -h now";
+		} else if (operatingSystem.contains("Windows")) {
+			shutdownCommand = "shutdown.exe -s -t 0";
+		} else {
+			throw new RuntimeException("Unsupported operating system...........");
+		}
 
-		    if ("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem)) {
-		        shutdownCommand = "shutdown -h now";
-		    }
-		    else if ("Windows".equals(operatingSystem)) {
-		        shutdownCommand = "shutdown.exe -s -t 0";
-		    }
-		    else {
-		        throw new RuntimeException("Unsupported operating system.");
-		    }
-
-		    Runtime.getRuntime().exec(shutdownCommand);
-		    System.exit(0);
-        return "hello";
-    }
+		Runtime.getRuntime().exec(shutdownCommand);
+		System.exit(0);
+		return "hello";
+	}
 }
