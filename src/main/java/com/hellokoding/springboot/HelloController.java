cf9a8408ead27219f8c24ca07a5a818fd2c7f343
Muzammil <mailmuzam@gmail.com>
2018-10-15 17:04:43 +0100

    Update HelloController.java

--- src/main/java/com/hellokoding/springboot/HelloController.java
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -7,21 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/shutdown")
+	@RequestMapping("/")
 	public String hello(Model model) throws IOException {
-		String shutdownCommand;
-		String operatingSystem = System.getProperty("os.name");
-		System.out.println(operatingSystem + "Operating sys");
-		if (operatingSystem.contains("Linux") || operatingSystem.contains("Mac OS X")) {
-			shutdownCommand = "shutdown -h now";
-		} else if (operatingSystem.contains("Windows")) {
-			shutdownCommand = "shutdown -s -t 0";
-		} else {
-			throw new RuntimeException("Unsupported operating system...........");
-		}
-
-		Runtime.getRuntime().exec(shutdownCommand);
-		System.exit(0);
+		
 		return "hello";
 	}
 }
